483c0916bffd38b878cafc510006f6d66d2de816
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-04-12 00:19:36 +0530

    added signout button

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { auth } from "../config/firebaseConfig";
+import { signOut } from "firebase/auth";
 
 function HomePage(){
 
+    
+    function signOutMethod(){
+        signOut(auth).then(()=>{
+            console.log("logged out successfully");
+        }).catch(e=>{
+            console.error(e);
+        })
+    }
+
+    const [user, setUser] = useState(false);
+    useEffect(()=>{
+      auth.onAuthStateChanged(user=>{
+        if(!user){
+          setUser(false);
+        }
+        else{
+          setUser(true);
+        }
+      });
+      console.log(auth.currentUser)
+    }, [])
+
     return(
-        <h1>Home page </h1>
+        <div>
+            
+            <h1>Home page </h1>
+            {user && <button onClick={signOutMethod}>signout</button>}
+        </div>
     );
 }
 
