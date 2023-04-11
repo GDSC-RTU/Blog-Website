e2d6425d6300e10d58fd0009eb1c2bd72808e2e3
Tapish1511 <87308840+Tapish1511@users.noreply.github.com>
2023-04-11 15:56:06 +0530

    browser Router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home";
+import SignUpPage from "./pages/signup";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/signup' element={<SignUpPage />}/>
+      </Routes>
+    </>
   );
 }
 
